7e9a937c28cf071517a52a3427e5ba21de74ba46
GuoFlight <1754811791@qq.com>
2023-06-12 21:47:27 +0800

    fix 将FLAG_GLOG_FILE设置成/dev/stdout会报错的问题

---
+++ change_log.md
@@ -0,0 +1,8 @@
+# v0.0
+
+* 初始版本
+
+# v0.1
+
+* fix 将FLAG_GLOG_FILE设置成"/dev/stdout"会报错的问题
+
--- glog_bash.sh
+++ glog_bash.sh
@@ -2,8 +2,8 @@
 
 ################################################
 # 作者：郭少
-# 版本：v0.0
-# 最近一次更新时间：2023-06-09
+# 版本：v0.1
+# 最近一次更新时间：2023-06-12
 ################################################
 
 
@@ -20,7 +20,7 @@ function init(){
     [ "${FLAG_GLOG_LEVEL}" == "${GLOG_LEVEL_WARN}" ] && GLOG_LEVEL=3
     [ "${FLAG_GLOG_LEVEL}" == "${GLOG_LEVEL_ERROR}" ] && GLOG_LEVEL=4
     # 初始化日志文件
-    if [ "${FLAG_GLOG_FILE}" != "" ]; then
+    if [ "${FLAG_GLOG_FILE}" != "" ] && [ "${FLAG_GLOG_FILE}" != "/dev/stdout"  ]; then
         GLOG_FILE=${FLAG_GLOG_FILE}
         if [ ! -e "${FLAG_GLOG_FILE}" ]; then
             glog_file_dir=`dirname ${FLAG_GLOG_FILE}`
@@ -41,7 +41,7 @@ function glog_debug(){
 }
 function glog_info(){
     [ ${GLOG_LEVEL} -gt 2 ] && return
-    print_log $(date +"%Y-%m-%d %H:%M:%S") [${GLOG_LEVEL_INFO}] $*
+    print_log $(date +"%Y-%m-%d %H:%M:%S") [${GLOG_LEVEL_INFO}] $1
 }
 function glog_warn(){
     [ "${GLOG_LEVEL}" -gt 3 ] && return
